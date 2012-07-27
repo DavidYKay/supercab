0bbb6c9c804eea073e06c24f29fe59df89f16517
David Young-Chan Kay <davidykay@gmail.com>
2012-07-27 18:22:44 +0300

    now properly testing for PickupPoint persistence.

--- SuperCab-Device-Test/src/co/gargoyle/supercab/android/test/DbSaveTest.java
+++ SuperCab-Device-Test/src/co/gargoyle/supercab/android/test/DbSaveTest.java
@@ -1,20 +1,31 @@
 package co.gargoyle.supercab.android.test;
 
+
+import java.util.List;
+import java.util.Locale;
+
+import android.content.ContentResolver;
 import android.content.Context;
+import android.location.Address;
+import android.test.AndroidTestCase;
 import android.test.InstrumentationTestCase;
+import android.test.IsolatedContext;
 import co.gargoyle.supercab.android.database.SCOrmLiteHelper;
+import co.gargoyle.supercab.android.enums.PointType;
+import co.gargoyle.supercab.android.model.PickupPoint;
 
 import com.j256.ormlite.android.apptools.OpenHelperManager;
+import com.j256.ormlite.dao.RuntimeExceptionDao;
 
-public class DbSaveTest extends InstrumentationTestCase {
+public class DbSaveTest extends AndroidTestCase {
 
-  private SCOrmLiteHelper mDatabaseHelper;
+  private SCOrmLiteHelper mHelper;
 
   @Override
   protected void tearDown() throws Exception {
-    if (mDatabaseHelper != null) {
+    if (mHelper != null) {
       OpenHelperManager.releaseHelper();
-      mDatabaseHelper = null;
+      mHelper = null;
     }
     super.tearDown();
   }
@@ -22,14 +33,32 @@ public class DbSaveTest extends InstrumentationTestCase {
   @Override
   protected void setUp() throws Exception {
     super.setUp();
-    if (mDatabaseHelper == null) {
-      Context context = getInstrumentation().getContext();
-      mDatabaseHelper = OpenHelperManager.getHelper(context, SCOrmLiteHelper.class);
+    if (mHelper == null) {
+
+//      Context context = getInstrumentation().getContext();
+//      ContentResolver resolver = context.getContentResolver();
+//      IsolatedContext isoContext = new IsolatedContext(resolver, context);
+
+      mHelper = OpenHelperManager.getHelper(getContext(), SCOrmLiteHelper.class);
     }
   }
 
   public void testDb() {
-    
-    assertTrue(1 == 1);
+    RuntimeExceptionDao<PickupPoint, Integer> dao = mHelper.getRuntimeDao(PickupPoint.class);
+
+    Address iHub = new Address(Locale.getDefault());
+    iHub.setLatitude(-1.29885);
+    iHub.setLongitude(36.79089);
+    iHub.setAddressLine(0,"iHub");
+
+    PickupPoint original = new PickupPoint(PointType.PICKUP, iHub);
+    dao.create(original);
+
+    List<PickupPoint> pickups = dao.queryForAll();
+
+    //PickupPoint persisted = dao.queryForFirst(preparedQuery);
+    PickupPoint persisted = pickups.get(0);
+
+    assertEquals(original, persisted);
   }
 }
